5c763531cd4134256d6ec0946df6de84a0eff9ad
Betty <betty.rangel.h@gmail.com>
2024-03-16 13:28:09 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Controllers/HelloWorldcController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace MVCApp.Controllers
+{
+    public class HelloWorldcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/HelloWorld/Index.cshtml");
+        }
+        public IActionResult Welcome(string name, int numTimes=1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View("~/Views/HelloWorld/Welcome.cshtml");
+
+        }
+
+        //public string Index()
+        //{
+        //    return "this is my default action...";
+        //}
+
+        //Get: /HelloWorldc/Welcome/
+        //Requieres using System.Text.Encoding.Web
+        //Get: /HelloWorldc/Welcome?name=betty&numtimes=5
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numTimes}");
+        //}
+
+    }
+}
---
+++ MVCApp/Models/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCApp.Data;
+using System;
+using System.Linq;
+
+namespace MVCApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MVCAppContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    context.Movie.AddRange(new Movie {
+                        Title = "",
+                        RealeseData = DateTime.Parse(""),
+                        Genre = "",
+                        Price =
+                    
+                    });
+                }
+            }
+        }
+
+    }
+}
